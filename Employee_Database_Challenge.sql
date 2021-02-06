23ab3d7d8f06372b88a09da9d0011ad8fa5a066c
Elijah Belizor <EliB@Elis-Macbook-Pro.local>
2021-02-06 12:26:07 -0600

    Challenge 7 Check

---
+++ Employee_Database_Challenge.sql
@@ -0,0 +1,47 @@
+SELECT e.emp_no
+	, e.first_name
+	, e.last_name
+	, ti.title
+	, ti.from_date
+	, ti.to_date
+INTO retirement_titles
+FROM employees as e
+FULL OUTER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no
+	, rt.first_name
+	, rt.last_name
+	, rt.title
+INTO unique_titles
+FROM retirement_titles AS rt
+ORDER BY rt.emp_no ASC, rt.to_date DESC;
+
+
+SELECT COUNT(ut.title)
+	, title
+INTO retiring_titles
+FROM unique_titles AS ut
+GROUP BY ut.title
+ORDER BY COUNT(ut.title) DESC;
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no
+	, e.first_name
+	, e.last_name
+	, e.birth_date
+	, de.from_date
+	, de.to_date
+	, ti.title
+INTO mentorship_eligibilty
+FROM employees AS e
+FULL OUTER JOIN dept_emp AS de
+	ON (e.emp_no = de.emp_no)
+	FULL OUTER JOIN titles as ti
+	ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+	   AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31');
+
+
