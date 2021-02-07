85fa3193586cd5d89634d0a40e979efcfb575425
Elijah Belizor <EliB@Elis-Macbook-Pro.local>
2021-02-06 20:09:04 -0600

    Challenge Check

--- Employee_Database_Challenge.sql
+++ Employee_Database_Challenge.sql
@@ -6,7 +6,7 @@ SELECT e.emp_no
 	, ti.to_date
 INTO retirement_titles
 FROM employees as e
-FULL OUTER JOIN titles AS ti
+INNER JOIN titles AS ti
 ON (e.emp_no = ti.emp_no)
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
@@ -37,7 +37,7 @@ SELECT DISTINCT ON (e.emp_no) e.emp_no
 	, ti.title
 INTO mentorship_eligibilty
 FROM employees AS e
-FULL OUTER JOIN dept_emp AS de
+INNER JOIN dept_emp AS de
 	ON (e.emp_no = de.emp_no)
 	FULL OUTER JOIN titles as ti
 	ON (e.emp_no = ti.emp_no)
